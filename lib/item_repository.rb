3ec2f15f64d5a55db59058e6085eab17988584a4
Erik Butcher <with_a_k@mac.com>
2015-04-21 17:23:57 -0600

    Item repository functionality and tests

---
+++ lib/item_repository.rb
@@ -0,0 +1,75 @@
+require 'smarter_csv'
+require_relative 'item'
+
+class ItemRepository
+  def initialize(filename = "./data/items.csv")
+    @csv_data = SmarterCSV.process(filename)
+    @items = @csv_data.map do |item| 
+      Item.new(item)
+    end
+  end
+
+  def all
+    @items
+  end
+
+  def random
+    @items.sample
+  end
+
+  def find_by_id(search_id)
+    @items.detect { |item| search_id == item.id }
+  end
+
+  def find_all_by_id(search_id)
+    @items.select { |item| search_id == item.id }
+  end
+
+  def find_by_name(search_name)
+    @items.detect { |item| search_name.downcase == item.name.downcase}
+  end
+
+  def find_all_by_name(search_name)
+    @items.select { |item| search_name.downcase == item.name.downcase }
+  end
+
+  def find_by_unit_price(search_price)
+    @items.detect { |item| search_price == item.unit_price}
+  end
+
+  def find_all_by_unit_price(search_price)
+    @items.select { |item| search_price == item.unit_price }
+  end
+
+  def find_by_description(search_description)
+    @items.detect { |item| search_description.downcase == item.description.downcase}
+  end
+
+  def find_all_by_description(search_description)
+    @items.select { |item| search_description.downcase == item.description.downcase }
+  end
+
+  def find_by_merchant_id(search_m_id)
+    @items.detect { |item| search_m_id == item.merchant_id}
+  end
+
+  def find_all_by_merchant_id(search_m_id)
+    @items.select { |item| search_m_id == item.merchant_id }
+  end
+
+  def find_by_created_at(search_time)
+    @items.detect { |item| search_time == item.created_at}
+  end
+
+  def find_all_by_created_at(search_time)
+    @items.select { |item| search_time == item.created_at }
+  end
+
+  def find_by_updated_at(search_time)
+    @items.detect { |item| search_time == item.updated_at}
+  end
+
+  def find_all_by_updated_at(search_time)
+    @items.select { |item| search_time == item.updated_at }
+  end
+end
---
+++ test/item_test.rb
@@ -0,0 +1,6 @@
+require 'minitest'
+require 'minitest/autorun'
+
+class ItemTest < Minitest::Test
+  
+end
