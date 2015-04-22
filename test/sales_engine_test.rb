1c2023e6ecced9f72541b206278c162f1be9be63
Josh Cass <joshuacass@gmail.com>
2015-04-22 14:18:48 -0600

    switch

--- test/sales_engine_test.rb
+++ test/sales_engine_test.rb
@@ -1,18 +1,18 @@
 gem 'minitest'
 require 'minitest/autorun'
 require 'minitest/pride'
-require './lib/customer_repository'
-require './lib/invoice_item_repository'
-require './lib/item_repository'
-require './lib/merchant_repository'
-require './lib/transaction_repository'
-require './lib/invoice_repository'
+require './lib/sales_engine'
 
 class SalesEngineTest < Minitest::Test
 
   def setup
-    dir = File.expand_path('test', __dir__)
-
+    dir = File.expand_path('test_data', __dir__)
+    test_engine = SalesEngine.new(dir)
+    test_engine.startup
   end
 
+  def test_merchant_items_method_returns_all_items_for_merchant_id
+    test_engine.merchant_repository.find_by_id(1)
+
+  end
 end
